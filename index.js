075bfc550ac9629e1df72e0dae392c89ede4c633
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-03-30 02:28:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9 '-]/g, '');
+  }
+
+  static titleize(str) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = str.split(' ');
+    const capitalizedWords = words.map(word => {
+      if (exceptions.includes(word)) {
+        return word;
+      } else {
+        return this.capitalize(word);
+      }
+    });
+    return this.capitalize(capitalizedWords.join(' '));
+  }
 }
